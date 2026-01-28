16cc70182e3757aa41f9d32816df7bdb4dc2bed1
Tomas <tomsipos27@gmail.com>
2026-01-28 09:34:36 +0100

    Vyhľadávanie podľa popisu

--- components/ZobrazenieVydavkov.js
+++ components/ZobrazenieVydavkov.js
@@ -1,16 +1,32 @@
-import { useContext } from "react";
-import { StyleSheet, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View, TextInput } from "react-native";
 import SumaVydavkov from "./SumaVydavkov";
 import ZoznamVydavkov from "./ZoznamVydavkov";
 
 import { GlobalStyles } from "../constant/styles";
-import { VydavkyContext } from "../store/vydavky-context";
 
 export default function ZobrazenieVydavkov({ vydavky, pocetDniVydavkov }) {
+  const [hladanyText, setHladanyText] = useState("");
+
+
+  const filtrovanéVydavky = vydavky.filter((vydavok) => {
+    return vydavok.popis.toLowerCase().includes(hladanyText.toLowerCase());
+  });
+
   return (
     <View style={styles.container}>
-      <SumaVydavkov vydavky={vydavky} pocetDni={pocetDniVydavkov} />
-      <ZoznamVydavkov vydavky={vydavky} />
+      <SumaVydavkov vydavky={filtrovanéVydavky} pocetDni={pocetDniVydavkov} />
+
+      
+      <TextInput
+        style={styles.vyhladavanie}
+        placeholder="Vyhľadať výdavok..."
+        placeholderTextColor={GlobalStyles.colors.primary100}
+        value={hladanyText}
+        onChangeText={setHladanyText}
+      />
+
+      <ZoznamVydavkov vydavky={filtrovanéVydavky} />
     </View>
   );
 }
@@ -20,5 +36,13 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary700,
+  },
+  vyhladavanie: {
+    backgroundColor: "white",
+    color: "black",
+    padding: 12,
+    borderRadius: 8,
+    marginVertical: 12,
+    fontSize: 16,
   },
 });
